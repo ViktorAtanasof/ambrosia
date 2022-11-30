4fe492068c406aab9d705d98f52582b2ecb77e35
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-30 15:34:32 +0200

    Add necessary imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RecipeModule } from './recipe/recipe.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    RecipeModule,
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
