2851152dffd9419dc317a6e7d077c02168f3c0bf
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-01 17:33:33 +0200

    Create error handlers and validations

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/validators';
+import { emailDomains } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +10,20 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
-  /* form = this.fb.group({
+  form = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(4)]],
-    email: ['', Validators.required, ]
-  }) */
+    email: ['', [Validators.required, appEmailValidator(emailDomains)]],
+    pass: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      repeatPassword: []
+    }, {
+      validators: [sameValueGroupValidator('password', 'repeatPassword')]
+    })
+  })
 
   constructor(private fb: FormBuilder) { }
 
+  registerHandler(): void {
+    console.log(this.form.value);
+  }
 }
