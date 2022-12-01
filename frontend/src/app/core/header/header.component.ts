dfa270aa545077c70cb467ea8effa78f32c775a7
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-01 17:32:30 +0200

    Verify user authentication

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,13 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-  constructor(private router: Router) { }
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
+  }
+
+  get user() {
+    return this.authService.user;
+  }
+
+  constructor(private router: Router, private authService: AuthService) { }
 }
