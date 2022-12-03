d799cc4fc830eda1747ef220b353a4c780d6b1de
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-03 19:41:26 +0200

    Create register handler

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/validators';
 import { emailDomains } from 'src/app/shared/constants';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -11,19 +13,20 @@ import { emailDomains } from 'src/app/shared/constants';
 export class RegisterComponent {
 
   form = this.fb.group({
-    username: ['', [Validators.required, Validators.minLength(4)]],
+    /* username: ['', [Validators.required, Validators.minLength(4)]], */
     email: ['', [Validators.required, appEmailValidator(emailDomains)]],
     pass: this.fb.group({
-      password: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       repeatPassword: []
     }, {
       validators: [sameValueGroupValidator('password', 'repeatPassword')]
     })
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private router: Router, private fb: FormBuilder, private authService: AuthService) { }
 
   registerHandler(): void {
-    console.log(this.form.value);
+    const value = this.form.value;
+    this.authService.register(value.email!, value.pass?.password!);
   }
 }
