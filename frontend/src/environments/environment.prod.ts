41ddc9a80f103e10bd8fca9246084734c620c589
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-03 19:32:13 +0200

    Configure firebase enviroment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ambrosia-d350f',
+    appId: '1:1067988830186:web:9bade4b5016fd448349493',
+    storageBucket: 'ambrosia-d350f.appspot.com',
+    apiKey: 'AIzaSyAUq5D7B9f4QmgVfwAvoGCv24XOw6vP0lY',
+    authDomain: 'ambrosia-d350f.firebaseapp.com',
+    messagingSenderId: '1067988830186',
+  },
   production: true,
   apiUrl: 'https://www.themealdb.com/api/json/v1/1'
 };
