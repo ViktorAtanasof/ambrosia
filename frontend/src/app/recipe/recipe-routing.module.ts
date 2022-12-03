2910e1119523f72c5f21650cc84e0dfdd9ab6fff
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-03 19:32:55 +0200

    Create routing for recipe details

--- frontend/src/app/recipe/recipe-routing.module.ts
+++ frontend/src/app/recipe/recipe-routing.module.ts
@@ -1,13 +1,19 @@
 import { RouterModule, Routes } from "@angular/router";
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
 import { RecipeItemsComponent } from "./recipe-items/recipe-items.component";
+import { RecipeResolver } from "./resolvers/recipe.resolver";
 
 const routes: Routes = [
     {
         path: 'recipes',
-        component: RecipeItemsComponent
-        /* children: [
-            
-        ] */
+        component: RecipeItemsComponent,
+    }, 
+    {
+        path: 'recipes/details/:id',
+        resolve: {
+            recipe: RecipeResolver
+        },
+        component: RecipeDetailsComponent
     }
 ];
 
