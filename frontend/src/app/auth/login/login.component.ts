f5a1f9aa603078bc6c520a9d9d570fe377ec3c94
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-03 19:43:35 +0200

    Create login form & login handler

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { emailDomains } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  form = this.fb.group({
+    email: ['', [Validators.required, appEmailValidator(emailDomains)]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
 
-  ngOnInit(): void {
+  constructor(private router: Router, private authService: AuthService, private fb: FormBuilder) { }
+
+  loginHandler(): void {
+    /* this.authService.user = {
+      username: 'Peter'
+    } as any; */
+    const value = this.form.value;
+    this.authService.login(value.email!, value.password!);
   }
-
 }
