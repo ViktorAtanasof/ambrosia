2895d81920d6f2cd5a7be97f5beba20daecc887d
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-04 19:07:43 +0200

    Make method for getting userID and create users firestore collection upon registration

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,48 +1,66 @@
 import { Injectable } from '@angular/core';
 import { IUser } from '../shared/interfaces';
 import { Router } from '@angular/router';
-import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,  } from '@angular/fire/auth';
+import {
+  Auth,
+  authState,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
   currentUser$ = authState(this.fireAuth);
-
-  constructor(private fireAuth: Auth, private router: Router) { }
-
-
-  register(email: string, password: string) {
-    createUserWithEmailAndPassword(this.fireAuth, email, password)
-      .then((response: any) => {
-        console.log(response);
-        this.router.navigate(['/']);
-      })
-      .catch((err) => {
-        alert(err.message);
-      })
+  
+  
+  getUID() {
+    return this.fireAuth.currentUser?.uid;
   }
+  constructor(private fireAuth: Auth, private router: Router, private afstore: AngularFirestore) { }
 
-  login(email: string, password: string) {
-    signInWithEmailAndPassword(this.fireAuth ,email, password)
-      .then((response: any) => {
-        console.log(response);
-        this.router.navigate(['/']);
-      })
-      .catch((err) => {
-        alert(err.message);
-      })
-  }
 
-  logout() {
-    signOut(this.fireAuth)
-      .then((response: any) => {
-        console.log(response);
-        this.router.navigate(['/']);
+register(email: string, password: string) {
+  createUserWithEmailAndPassword(this.fireAuth, email, password)
+    .then((res: any) => {
+      this.afstore.doc(`users/${res.user.uid}`).set({
+        email
       })
-      .catch((err) => {
-        alert(err.message);
-      })
-  }
+      this.router.navigate(['/']);
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+}
+
+login(email: string, password: string) {
+  signInWithEmailAndPassword(this.fireAuth, email, password)
+    .then((res: any) => {
+      if (res.user) {
+        console.log(res.user.uid);
+
+      }
+      this.router.navigate(['/']);
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+}
+
+logout() {
+  signOut(this.fireAuth)
+    .then((response: any) => {
+      console.log(response);
+      this.router.navigate(['/']);
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+}
 }
