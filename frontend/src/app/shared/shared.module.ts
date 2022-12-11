4d7d37e7600779056d09237a13bb8162cd48df92
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-11 11:42:16 +0200

    Import & export shorten pipe and spinner component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AppEmailDirective } from './validators';
+import { ShortenPipe } from './pipes/shorten.pipe';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
-    AppEmailDirective
+    AppEmailDirective,
+    ShortenPipe,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
     RouterModule
+  ],
+  exports: [
+    ShortenPipe,
+    SpinnerComponent
   ]
 })
 export class SharedModule { }
