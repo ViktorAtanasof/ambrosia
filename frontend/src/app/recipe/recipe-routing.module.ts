65d7565392acd4e8bef7db3565071e5d1f1ddcea
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-11 11:47:10 +0200

    Create route path for recipe-randomizer component

--- frontend/src/app/recipe/recipe-routing.module.ts
+++ frontend/src/app/recipe/recipe-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from "@angular/router";
 import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
 import { RecipeItemsComponent } from "./recipe-items/recipe-items.component";
+import { RecipeRandomizerComponent } from "./recipe-randomizer/recipe-randomizer.component";
 import { RecipeResolver } from "./resolvers/recipe.resolver";
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
             recipe: RecipeResolver
         },
         component: RecipeDetailsComponent
+    },
+    {
+        path: 'random-recipe',
+        component: RecipeRandomizerComponent,
     }
 ];
 
