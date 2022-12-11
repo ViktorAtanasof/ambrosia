257ca1affd3072ca881a296e0cd6804a666684d8
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-11 11:52:08 +0200

    Create good UI/UX for recipe-details component and add logic for: - Checking if user has favourited the following recipe - Store only necessary recipe information - Create a method for adding recipes to favourites

--- frontend/src/app/recipe/recipe-details/recipe-details.component.ts
+++ frontend/src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import { ApiService } from 'src/app/api.service';
+import { AuthService } from 'src/app/auth/auth.service';
+
+const firestore = firebase.firestore;
 
 @Component({
   selector: 'app-recipe-details',
@@ -9,14 +15,49 @@ import { ActivatedRoute } from '@angular/router';
 export class RecipeDetailsComponent implements OnInit {
 
   details: any
+  recipeId: any;
+  isSubscribed: boolean | undefined;
+  recipeInfo : any;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private afstore: AngularFirestore,
+    public authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(data => {
       this.details = data['recipe']['meals'][0];
-      console.log(this.details);
+      this.recipeId = this.details.idMeal;
+    });
+    this.checkForSubscription();
+    this.recipeInfo = {
+      idMeal: this.details.idMeal,
+      strMeal: this.details.strMeal,
+      strMealThumb: this.details.strMealThumb,
+      strInstructions: this.details.strInstructions
+    }
+  }
+
+  checkForSubscription() {
+    this.afstore.collection('users').get().subscribe((snapshot) => {      
+      snapshot.docs.forEach((doc: any) => {
+        if(doc.data().favourites !== undefined) {
+          if (doc.id === this.authService.getUID() 
+          && doc.data().favourites.some((e: { idMeal: any; }) => e.idMeal === this.recipeId)) {
+            this.isSubscribed = true;
+          }  
+        }
+      })
     })
+  }
+
+  addToFavourites() {
+    this.afstore.doc(`users/${this.authService.getUID()}`).update({
+      favourites: firestore.FieldValue.arrayUnion(this.recipeInfo)
+    });
+    this.checkForSubscription();
   }
 
 }
