dafeaca5bf9a336d181765e78a7f97fa9fc8c47d
ViktorAtanasof <atanasowviktor@gmail.com>
2022-12-11 11:44:53 +0200

    Add spinner and display specific text if the user doesn't have favourited recipes

--- frontend/src/app/auth/profile/profile.component.ts
+++ frontend/src/app/auth/profile/profile.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NavigationEnd, Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import { AuthService } from '../auth.service';
+
+const firestore = firebase.firestore;
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userFavourites: any;
+  populateFavourites: any;
+  userRef: any;
+  sub: any;
+
+  constructor(
+    private afstore: AngularFirestore,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    };
+    this.sub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Here is the dashing line comes in the picture.
+        // You need to tell the router that, you didn't visit or load the page previously, so mark the navigated flag to false as below.
+        this.router.navigated = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
+    //Getting user favourited recipes
+    this.userRef = this.afstore.doc(`users/${this.authService.getUID()}`);
+    this.userRef.get().forEach((doc: any) => {
+      this.userFavourites = doc.data().favourites;
+    });
+  }
+
+  removeFromFavourites(currId: string) {
+    let confirmMessage = 'Are you sure you want to delete this recipe?';
+    if(confirm(confirmMessage) == true) {
+      this.userRef.get().subscribe((doc: any) => {
+        doc.data().favourites.forEach((e: any) => {
+          if (e.idMeal === currId) {
+            this.userRef.update({
+              favourites: firestore.FieldValue.arrayRemove(e)
+            });
+            this.router.navigate(['/profile'])
+          }
+        })
+      })
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
