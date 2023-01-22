e72100f0738b624de5097e52a992fd40b67cde03
ViktorAtanasof <atanasowviktor@gmail.com>
2023-01-22 16:41:31 +0200

    Create dynamic search functionality

--- frontend/src/app/recipe/recipe-items/recipe-items.component.ts
+++ frontend/src/app/recipe/recipe-items/recipe-items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
 import { IRecipe } from 'src/app/shared/interfaces';
 
@@ -10,14 +12,38 @@ import { IRecipe } from 'src/app/shared/interfaces';
 export class RecipeItemsComponent implements OnInit {
 
   recipes: IRecipe[] | undefined;
+  searchValue: string | undefined;
 
-  constructor(private apiService: ApiService) { }
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl('')
+  })
+
+  constructor(private apiService: ApiService) {
+    this.searchForm.get('search')?.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap((v) => this.apiService.searchByMainIngredient(v))
+      )
+      .subscribe({
+        next: (value) => {
+          // Object -> meals[Objects]
+          this.recipes = value.meals;
+          this.searchValue = this.searchForm.get('search')?.value;
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+  }
 
   ngOnInit(): void {
     this.apiService.loadAllRecipes().subscribe({
       next: (value) => {
         // Object -> meals[Objects]
-        this.recipes = value.meals;
+        if (value.meals.length > 12) {
+          this.recipes = value.meals.splice(0, 12);
+        }
       },
       error: (err) => {
         console.error(err);
